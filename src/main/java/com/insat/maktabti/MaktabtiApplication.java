56093b561e9b993a95d79ac75665b6b2147bca94
Bahaabrougui <bouhaa7@gmail.com>
2019-11-24 13:32:27 +0100

    init

---
+++ src/main/java/com/insat/maktabti/MaktabtiApplication.java
@@ -0,0 +1,13 @@
+package com.insat.maktabti;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MaktabtiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MaktabtiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/insat/maktabti/MaktabtiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.insat.maktabti;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MaktabtiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
