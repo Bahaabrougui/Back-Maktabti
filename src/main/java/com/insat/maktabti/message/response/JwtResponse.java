276b813c3b2ca8d4585f710665b4326fcc263f84
hawejdene <hawejdene@gmail.com>
2019-12-01 20:24:18 +0100

    fix jwt response

--- src/main/java/com/insat/maktabti/message/response/JwtResponse.java
+++ src/main/java/com/insat/maktabti/message/response/JwtResponse.java
@@ -1,5 +1,6 @@
 package com.insat.maktabti.message.response;
 
+import com.insat.maktabti.domain.User;
 import org.springframework.security.core.GrantedAuthority;
 
 import java.util.Collection;
@@ -7,12 +8,40 @@ import java.util.Collection;
 public class JwtResponse {
     private String token;
     private String type = "Bearer";
-    private String username;
+    private User user;
     private Collection<? extends GrantedAuthority> authorities;
 
-    public JwtResponse(String accessToken, String username, Collection<? extends GrantedAuthority> authorities) {
+    public JwtResponse(String accessToken, User user, Collection<? extends GrantedAuthority> authorities) {
         this.token = accessToken;
-        this.username = username;
+        this.user = user;
+        this.authorities = authorities;
+    }
+
+    public String getToken() {
+        return token;
+    }
+
+    public void setToken(String token) {
+        this.token = token;
+    }
+
+    public String getType() {
+        return type;
+    }
+
+    public void setType(String type) {
+        this.type = type;
+    }
+
+    public User getUser() {
+        return user;
+    }
+
+    public void setUser(User user) {
+        this.user = user;
+    }
+
+    public void setAuthorities(Collection<? extends GrantedAuthority> authorities) {
         this.authorities = authorities;
     }
 
@@ -32,13 +61,7 @@ public class JwtResponse {
         this.type = tokenType;
     }
 
-    public String getUsername() {
-        return username;
-    }
 
-    public void setUsername(String username) {
-        this.username = username;
-    }
 
     public Collection<? extends GrantedAuthority> getAuthorities() {
         return authorities;
